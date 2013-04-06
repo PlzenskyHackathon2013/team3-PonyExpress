d150264cd50d6378f3c76bf270dbfd9b85269fd8
hduong <zerox414@gmail.com>
2013-04-06 23:44:25 +0200

    Move toJSON(), parse() from Collection to ModelIndex

--- js/collection/indexes.js
+++ js/collection/indexes.js
@@ -7,16 +7,6 @@ define(['backbone', 'model/index', 'config'], function (Backbone, ModelIndex, co
 		url: function () {
 			
 		},
-        
-        toJSON: function(options) {
-            var encrypted = CryptoJS.AES.encrypt(JSON.stringify(this.attributes), this.options.password);
-            return encrypted.toString();
-        },
-        parse: function(response, options) {
-            var decrypted = CryptoJS.AES.decrypt(JSON.parse(response), this.options.password);
-            return decrypted.toString(CryptoJS.enc.Utf8);
-        }
-
 	};
 
 	return Backbone.Collection.extend(CollectionIndexes);
--- js/model/index.js
+++ js/model/index.js
@@ -1,9 +1,19 @@
 define(['backbone', 'config'], function (Backbone, config) {
 
 	var ModelIndex = {
-
+        url: function () {
+			return "";
+		},
+    
+        toJSON: function(options) {
+            var encrypted = CryptoJS.AES.encrypt(JSON.stringify(this.attributes), this.options.password);
+            return encrypted.toString();
+        },
+        parse: function(response, options) {
+            var decrypted = CryptoJS.AES.decrypt(JSON.parse(response), this.options.password);
+            return decrypted.toString(CryptoJS.enc.Utf8);
+        }
 	};
-
 	return Backbone.Model.extend(ModelIndex);
 
 });
