e157a1c0f7e70368648b96ea543c4de8e7fc6856
Petr Blahník <blahnikpetr@gmail.com>
2013-04-07 01:29:10 +0200

    modified route

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
+//mock username, it will be replace by auth
+var username="Rainbow Dash";
 
 /**
  * Module dependencies.
  */
 
-var username="Rainbow Dash";
-
+// define variable
 var express = require('express')
   , routes = require('./routes')
   , index = require('./routes/index')
@@ -18,7 +19,7 @@ app.configure(function() {
   app.use(express.bodyParser());
 });
 
-// all environments
+//set enviroment
 app.set('ipaddress', process.env.OPENSHIFT_INTERNAL_IP);
 app.set('port', process.env.OPENSHIFT_INTERNAL_PORT || 8080);
 app.set('views', __dirname + '/views');
@@ -29,7 +30,6 @@ app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(require('less-middleware')({ src: __dirname + '/public' }));
-
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/lib', express.static(path.join(__dirname, 'lib')));
 app.use('/js', express.static(path.join(__dirname, 'js')));
@@ -39,21 +39,35 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-
-app.get('/password', password.storage);
-
-app.get('/password/:id', password.password);
-console.log(JSON.stringify(password));
-
-//add new user
-app.delete('/list', index.delete);
-app.delete('/list/:id', index.deleteById);
-app.post('/list/:username', index.addUser);
-app.put('/list//:id', index.updateById);
+//get all list
 app.get('/list/', index.list);
-app.get('/index', index.findAll);
 
+//add new hash
+app.post('/list/', index.add);
+
+// edit hash
+// change to post
+app.put('/list/:id', index.updateById);
+
+// delete all record
+app.delete('/list', index.delete);
+
+// delete on hash by id
+app.delete('/list/:id', index.deleteById);
+
+//get file by id
+app.get('/storage/:id', storage.getById);
+
+//add new file
+app.post('/storage/', storage.add);
+
+// update file by id
+app.post('/storage/:id', storage.updateById);
+
+// delete file by id
+app.delete('/storage/:id', storage.deleteById);
+
+//create server
 http.createServer(app).listen(app.get('port'), app.get('ipaddress'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
