1b2ad4b6a306a7efc0a27eecee4ae527be5621e7
hduong <zerox414@gmail.com>
2013-04-06 22:20:59 +0200

    Implemented toJSON() and parse() for CollectionIndexes

--- js/collection/indexes.js
+++ js/collection/indexes.js
@@ -4,7 +4,16 @@ define(['backbone'], function (Backbone) {
 
 		url: function () {
 			//resolve url
-		}
+		},
+        
+        toJSON: function(options) {
+            var encrypted = CryptoJS.AES.encrypt(JSON.stringify(this.attributes), this.options.password);
+            return encrypted.toString();
+        },
+        parse: function(response, options) {
+            var decrypted = CryptoJS.AES.decrypt(JSON.parse(response), this.options.password);
+            return decrypted.toString(CryptoJS.enc.Utf8);
+        }
 
 	};
 
