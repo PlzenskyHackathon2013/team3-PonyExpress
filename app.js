c04245b0d61d6be9c1c97be76f2e76d1e9f4d591
Pony master <michal.bryxi@gmail.com>
2013-04-06 16:17:14 +0200

    It is alive!

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+app.get('/mirek', user.findAll);
 
 http.createServer(app).listen(app.get('port'), app.get('ipaddress'), function(){
   console.log('Express server listening on port ' + app.get('port'));
